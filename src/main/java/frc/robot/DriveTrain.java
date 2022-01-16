7c0caf4a20b0dea81f38e7d67a71479b2490cb46
Stephen David <sdavid14@gmail.com>
2022-01-15 19:08:35 -0500

    testing

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -1,19 +1,23 @@
 package frc.robot;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class DriveTrain {
     PIDController _pid;
+    Encoder _enc;
+
     int _kP;
     int _kI;
     int _kD;
    
     public DriveTrain(int kP, int kI, int kD) {
         // Creates a PIDController with gains kP, kI, and kD
-        this(new PIDController(kP, kI, kD));
+        this(new PIDController(kP, kI, kD), new Encoder(1, 2));
     }
 
-    public DriveTrain(PIDController p) {
+    public DriveTrain(PIDController p, Encoder e) {
         _pid = p;
+        _enc = e;
     }
 }
